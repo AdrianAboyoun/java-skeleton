51f22a2c6e685817a1d8bb8b8fd69cf53068c408
AdrianAboyoun <adrian.aboyoun@gmail.com>
2018-10-31 18:25:51 +0000

    first attempt

--- src/main/java/answers/Question4.java
+++ src/main/java/answers/Question4.java
@@ -3,8 +3,23 @@ package answers;
 public class Question4 {
 
 	public static int selectionFailedTradedesks(String[][] rows, int numberMachines) {
-		// TODO Auto-generated method stub
-		return -1;
+		int[] totals = new int[rows.length];
+		for (int i = 0; i < rows.length; i++) {
+			int rowTotal = 0;
+			for (int j = 0; j < rows[i].length; j++) {
+				if (rows[i][j] != "X") {
+					rowTotal += Integer.parseInt(rows[i][j]);
+				}
+			}
+			totals[i] = rowTotal;
+		}
+		int min = 0;
+		for (int i = 0; i < totals.length; i++) {
+			if (min <= totals[i]) {
+				min = totals[i];
+			}
+		}
+		return min;
 	}
 
 }
